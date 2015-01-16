efab3094f83e8cd6917c9cdcce2384bf11f71701
Athaboros <athuggins@gmail.com>
2015-01-16 09:28:59 -0500

    implement final function, clean and add comments

--- syllabus_to_html.py
+++ syllabus_to_html.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 '''
 #  Not doing this part because urllib2 doesn't support ssl/https.
 import urllib2
-import re
 
 def get_course_from_url():
     """
@@ -18,6 +17,7 @@ def get_course_from_url():
         if re.findall('syllabus', a['href']):
             print "Found the Syllabus:", a['href']
 '''
+
 
 def read_course_html(course):
     """
@@ -38,10 +38,12 @@ def find_course_units(course_file):
     unit_list = []
 
     for i in range(20):
+        # find each unit using the div tag id 'X_unit'
         unit_x = course_file.find(id='%s_unit' % i)
         if unit_x is None:
             pass
         elif len(unit_x) > 0:
+            # convert the contents of the unit to a string, add it to the unit list
             unit_x = str(unit_x)
             unit_list.append(unit_x)
     return unit_list
@@ -54,11 +56,13 @@ def write_unit_to_file(unit_list, file_format):
     """
     count = 0
     for i in unit_list:
-        to_write = str(i)  # [1:-1] was not necessary
+        to_write = str(i)
         count += 1
         if count <= 9:
+            # write Unit01-09.html/md, for units 1-9
             output = open('Unit0%d.%s' % (count, file_format), 'w')
         else:
+            # write Unit10-20.html/md, for units 10-20
             output = open('Unit%d.%s' % (count, file_format), 'w')
         output.write(to_write)
         output.close()
@@ -70,11 +74,14 @@ def html_to_markdown(num_of_units):
     Converts HTML to markdown based on how many units a course has.
     """
     for count in range(1, num_of_units + 1):
-        output = open('Unit0%d.md' % count, 'w')
+        # the if/else is necessary so that the unit files are formatted properly
+        # and you don't get Unit1.html or Unit010.html
         if count <= 9:
+            output = open('Unit0%d.md' % count, 'w')
             to_write = pypandoc.convert("Unit0%d.html" % count, "markdown_strict").encode('utf-8')
         else:
-            to_write = pypandoc.convert("Unit0%d.html" % count, "markdown_strict").encode('utf-8')
+            output = open('Unit%d.md' % count, 'w')
+            to_write = pypandoc.convert("Unit%d.html" % count, "markdown_strict").encode('utf-8')
         output.write(to_write)
         output.close()
 
@@ -86,11 +93,15 @@ def get_course_intro(course_file):
     whole_course = str(course_file)
     new_whole_course = whole_course.split('\n')
     for line in new_whole_course:
+        # course syllabus files alwyas begin with this line
         if 'Course Syllabus for' in line:
             start_line = new_whole_course.index(line)
+        # course intros stop here, where the 'Course Overview' section begins
         if 'id="overview"' in line:
             end_line = new_whole_course.index(line)
+    # puts each line of the html file into a big list
     intro_list = new_whole_course[start_line:end_line]
+    # brings the list back together into a string with each line of the html on a new line
     intro_string = '\n'.join(intro_list)
     return intro_string
 
@@ -108,18 +119,36 @@ def course_intro_to_html_and_md(intro_string):
     md_output.write(to_write)
     md_output.close()
 
-# Okay, this next part is a mess, but I'll clean it up next and make it its own function that will just take the
-# html file of the syllabus and output everything else.
 
-course_name = "arth110"
-# course_name = str(raw_input("What is the name of the course file you'd like to process? "))
-if not os.path.exists(course_name):
-    os.makedirs(course_name)
-course_doc = read_course_html("%s.html" % course_name)
-course_units = find_course_units(course_doc)
-os.chdir(course_name)
-unit_count = write_unit_to_file(course_units, "html")
-#  writes each unit to an html file, and returns how many units there are so pandoc knows what to do
-html_to_markdown(unit_count)
-intro = get_course_intro(course_doc)
-course_intro_to_html_and_md(intro)
+
+def leroy_jenkins():
+    """
+    Let's do this.
+    """
+    course_name = str(raw_input("What is the name of the course file you'd like to process? "))
+
+    # makes a new directory based on the course's name
+    if not os.path.exists(course_name):
+        os.makedirs(course_name)
+
+    # opens the course html file
+    course_doc = read_course_html("%s.html" % course_name)
+
+    # finds how many units the course has
+    course_units = find_course_units(course_doc)
+
+    # goes to the course directory
+    os.chdir(course_name)
+
+    # writes an html file for each unit, then converts each one to markdown
+    unit_count = write_unit_to_file(course_units, "html")
+    html_to_markdown(unit_count)
+
+    # processes the course intro
+    intro = get_course_intro(course_doc)
+    course_intro_to_html_and_md(intro)
+
+    print "All done! Check the '%s' folder for all the files for your course." % course_name
+
+
+leroy_jenkins()
